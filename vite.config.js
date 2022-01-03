e83bace1ad8e8722be6731c4d7f8bc54ad065864
titangene <titangene.tw@gmail.com>
2022-01-04 07:41:47 +0800

    chore: use vite-plugin-eslint

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath } from 'url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
