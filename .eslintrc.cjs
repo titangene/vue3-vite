fcd1a31a10fd72fc09083f56dde5296915899fa9
titangene <titangene.tw@gmail.com>
2022-01-04 12:04:22 +0800

    chore: remove ESLint extend config `plugin:vue/vue3-essential`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
     'plugin:vue/vue3-strongly-recommended',
     'eslint:recommended',
     '@vue/eslint-config-prettier'
