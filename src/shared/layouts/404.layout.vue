b609dfb65c02c1f3a37d28edf93106509422c31e
titangene <titangene.tw@gmail.com>
2022-01-04 18:01:53 +0800

    feat: 404 layout

---
+++ src/shared/layouts/404.layout.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
+    <RouterLink :to="{ name: 'About' }">About</RouterLink>
+  </nav>
+  <h1>404</h1>
+  <RouterView />
+</template>
