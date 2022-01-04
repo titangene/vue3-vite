f328b8635df95ff291d8cc6cf5a977c5f06e3062
titangene <titangene.tw@gmail.com>
2022-01-04 10:56:51 +0800

    style: prettier format

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,13 @@ import HelloWorld from '@/components/HelloWorld.vue';
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
 
-  <router-view />
+  <RouterView />
 </template>
 
 <style lang="scss">
