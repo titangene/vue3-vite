e2c99ba869f7d352d1ee08a055c83e5b224b7baf
titangene <titangene.tw@gmail.com>
2022-01-07 15:28:28 +0800

    refactor: default layout styles

--- src/shared/layouts/Default.layout.vue
+++ src/shared/layouts/Default.layout.vue
@@ -32,37 +32,40 @@ import HelloWorld from '@/shared/components/HelloWorld.vue';
   <RouterView />
 </template>
 
-<style lang="scss">
-@import '@/assets/base.css';
-
-header {
-  @include breakpoint.media(sm) {
-    color: color.$black;
-  }
-  @include breakpoint.media(md) {
-    color: color.$red-500;
-  }
-  @include breakpoint.media(lg) {
-    color: color.$blue-500;
-  }
-}
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
+<style lang="scss" scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
+
+  @include breakpoint.media(sm) {
+    color: color.$black;
+  }
+
+  @include breakpoint.media(md) {
+    color: color.$red-500;
+  }
+
+  @include breakpoint.media(lg) {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+    color: color.$blue-500;
+
+    .wrapper {
+      display: flex;
+      place-items: flex-start;
+      flex-wrap: wrap;
+    }
+  }
 }
 
 .logo {
   display: block;
   margin: 0 auto 2rem;
+
+  @include breakpoint.media(lg) {
+    margin: 0 2rem 0 0;
+  }
 }
 
 a,
@@ -83,61 +86,32 @@ nav {
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
-}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
+  @include breakpoint.media(lg) {
     text-align: left;
     margin-left: -1rem;
     font-size: 1rem;
 
     padding: 1rem 0;
     margin-top: 1rem;
+  }
+
+  a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+
+    &:first-of-type {
+      border: 0;
+    }
+  }
+
+  a.router-link-exact-active {
+    color: var(--color-text);
+
+    &:hover {
+      background-color: transparent;
+    }
   }
 }
 </style>
