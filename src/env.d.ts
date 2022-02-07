5b2db726e330de1d9a60d4a633221caa8ebe04d6
titangene <titangene.tw@gmail.com>
2022-02-07 14:41:32 +0800

    build: add VITE_BASE_PUBLIC_PATH env

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string
+  readonly VITE_BASE_PUBLIC_PATH: string
 }
 
 interface ImportMeta {
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 import eslint from 'vite-plugin-eslint';
@@ -11,75 +11,80 @@ import legacy from '@vitejs/plugin-legacy';
 import Inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
-    quasar({
-      autoImportComponentCase: 'pascal',
-      sassVariables: false
-    }),
-    eslint({
-      cache: false,
-      exclude: ['dist']
-    }),
-    Pages({
-      extensions: ['vue'],
-      pagesDir: [{ dir: 'src/modules/**/pages', baseRoute: '' }]
-    }),
-    Layouts({
-      defaultLayout: 'Default.layout',
-      layoutsDir: 'src/shared/layouts'
-    }),
-    svgLoader(),
-    legacy({
-      targets: [
-        '> 1%',
-        'last 2 versions',
-        'not dead',
-        'chrome >= 77',
-        'safari >= 13',
-        'ios_saf >= 13'
-      ]
-    }),
-    Inspect({
-      // change this to enable inspect for debugging
-      enabled: false
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @use 'src/styles/base/color';
-          @use 'src/styles/base/typography';
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.');
 
-          @use 'src/styles/utils/breakpoint';
-        `
+  return {
+    base: env.VITE_BASE_PUBLIC_PATH ?? '/',
+    plugins: [
+      vue({
+        template: { transformAssetUrls }
+      }),
+      quasar({
+        autoImportComponentCase: 'pascal',
+        sassVariables: false
+      }),
+      eslint({
+        cache: false,
+        exclude: ['dist']
+      }),
+      Pages({
+        extensions: ['vue'],
+        pagesDir: [{ dir: 'src/modules/**/pages', baseRoute: '' }]
+      }),
+      Layouts({
+        defaultLayout: 'Default.layout',
+        layoutsDir: 'src/shared/layouts'
+      }),
+      svgLoader(),
+      legacy({
+        targets: [
+          '> 1%',
+          'last 2 versions',
+          'not dead',
+          'chrome >= 77',
+          'safari >= 13',
+          'ios_saf >= 13'
+        ]
+      }),
+      Inspect({
+        // change this to enable inspect for debugging
+        enabled: false
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        // https://rollupjs.org/guide/en/#outputmanualchunks
-        manualChunks(id) {
-          if (id.includes('src/modules/users')) return 'users';
-          if (id.includes('src/modules/posts')) return 'posts';
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use 'src/styles/base/color';
+            @use 'src/styles/base/typography';
+
+            @use 'src/styles/utils/breakpoint';
+          `
         }
       }
     },
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
+    build: {
+      rollupOptions: {
+        output: {
+          // https://rollupjs.org/guide/en/#outputmanualchunks
+          manualChunks(id) {
+            if (id.includes('src/modules/users')) return 'users';
+            if (id.includes('src/modules/posts')) return 'posts';
+          }
+        }
+      },
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
       }
     }
-  }
+  };
 });
