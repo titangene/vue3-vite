11f41d2c61375b17f1a8547da0b4c2b2b4b2eead
titangene <titangene.tw@gmail.com>
2022-02-10 09:25:50 +0800

    feat: Quasar icon set use bootstrap svg icons

--- src/plugins/quasar.js
+++ src/plugins/quasar.js
@@ -1,14 +1,15 @@
 import { Quasar } from 'quasar';
 import zhTW from 'quasar/lang/zh-TW';
 
-import '@quasar/extras/material-icons/material-icons.css';
+import iconSet from 'quasar/icon-set/svg-bootstrap-icons';
 
 import 'quasar/src/css/index.sass';
 
 export default {
   install(app) {
     app.use(Quasar, {
-      lang: zhTW
+      lang: zhTW,
+      iconSet
     });
   }
 };
